0c0be422a2055ceac00237fc3073ab8a4eeaadb8
Tenzin Dekyong <dhontenzin@gmail.com>
2023-07-13 22:41:28 +0200

    user model created and app js modifs

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 require('dotenv').config();
+require('express-async-errors');
 
 // express
 const express = require('express');
 const app = express();
 
+// rest of the packages
+const morgan = require('morgan');
+
 // db
 const connectDb = require('./db/connect');
 
+// middlewares
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('hi tenzin'));
+app.use(morgan('tiny'));
+app.use(express.json()); // to access json in req.body
+
+app.get('/', (req, res) => {
+  res.send('ecommerce');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
