1631a0a4f2fe90af23c09bff68ad8ad843c71657
Tenzin Dekyong <dhontenzin@gmail.com>
2023-07-25 13:24:04 +0200

    review models, routes and create functionality done

---
+++ controllers/reviewController.js
@@ -0,0 +1,46 @@
+const Review = require('../models/Review');
+const Product = require('../models/Product');
+const { checkPermissions } = require('../utils');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
+const createReview = async (req, res) => {
+  const { product: productId } = req.body;
+
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`no product with id: ${productId}`);
+  }
+
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadySubmitted) {
+    throw new CustomError.BadRequestError(`Already submitted review for this product`);
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
+};
+
+const getAllReviews = async (req, res) => {
+  res.send('getAllReviews');
+};
+
+const getSingleReview = async (req, res) => {
+  res.send('getSingleReview');
+};
+
+const updateReview = async (req, res) => {
+  res.send('updateReview');
+};
+
+const deleteReview = async (req, res) => {
+  res.send('deleteReview');
+};
+
+module.exports = { createReview, getAllReviews, getSingleReview, updateReview, deleteReview };
---
+++ models/Review.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Please provide a review title'],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, 'Please provide a review text'],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// user can review one product only once
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model('Review', ReviewSchema);
