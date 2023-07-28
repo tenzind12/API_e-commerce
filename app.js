9567a2c61d1b1df52002b186ac64bd0ba3eba369
Tenzin Dekyong <dhontenzin@gmail.com>
2023-07-28 23:03:46 +0200

    some security packages for nodejs

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // db
 const connectDb = require('./db/connect');
@@ -25,6 +30,18 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const port = process.env.PORT || 5000;
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan('tiny'));
 app.use(express.json()); // to access json in req.body
